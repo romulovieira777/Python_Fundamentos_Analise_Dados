33f7b0a081acfd61cdf7540fa470f1bb12a78b13
Romulo Vieira <romulo.vieira777@gmail.com>
2022-02-08 12:54:54 -0300

    Loops, Condicionais, Métodos e Funções

--- Seção 03 - Loops, Condicionais, Métodos e Funções/Laboratório/calculadora_v1.py
+++ Seção 03 - Loops, Condicionais, Métodos e Funções/Laboratório/calculadora_v1.py
@@ -6,4 +6,20 @@
 
 print("\n******************* Python Calculator *******************")
 
+print("Selecione o número da operação desejada:\n 1 - Soma\n 2 - Subtração\n 3 - Multiplicação\n 4 - Divisão")
+opcao = int(input("Digite sua opção (1/2/3/4): "))
 
+number_01 = int(input("Digite o primeiro número: "))
+number_02 = int(input("Digite o segundo número: "))
+
+
+if opcao == 1:
+   print(f'{number_01} + {number_02} = {number_01 + number_02}')
+elif opcao == 2:
+    print(f'{number_01} - {number_02} = {number_01 - number_02}')
+elif opcao == 3:
+    print(f'{number_01} * {number_02} = {number_01 * number_02}')
+elif opcao == 4:
+    print(f'{number_01} / {number_02} = {number_01 / number_02}')
+else:
+    print("Selectione uma das opções válidas")
